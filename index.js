b56044abcfd6d241692148204605cd7417ed1b0f
Tupiet <Tupiet@users.noreply.github.com>
2020-05-13 19:23:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function canvi() {
-  var backDiv = document.getElementById("backgroundDiv");
-  backgroundDiv.style.backgroundColor = "#AA00AA";
-
+  //var backDiv = document.getElementById("backgroundDiv");
+  document.body.style.backgroundColor = "#AA0000";
+  if (document.body.style.backgroundColor == "#AA0000") {
+    //alert("AA0000");
+    document.body.style.backgroundColor = "#00AA00";
+    setTimeout(canvi(), 3000);
+  } else {
+    //alert("NO");
+    document.body.style.backgroundColor = "#AA0000";
+    setTimeout(canvi(), 3000);
+  }
 
 }
