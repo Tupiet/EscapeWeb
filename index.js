60810db989ffab6df09e208495fff2c99f31e17a
Tupiet <Tupiet@users.noreply.github.com>
2020-05-13 17:20:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function canvi() {
 
   if (document.body.style.backgroundColor == "#AA0000") {
-    alert("AA0000");
+    //alert("AA0000");
     document.body.style.backgroundColor = "#00AA00";
-    setTimeout(canvi(), 1000);
+    setTimeout(canvi(), 3000);
   } else {
-    alert("NO");
+    //alert("NO");
     document.body.style.backgroundColor = "#AA0000";
-    setTimeout(canvi(), 1000);
+    setTimeout(canvi(), 3000);
   }
 
 }
