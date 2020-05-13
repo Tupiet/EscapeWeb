2beef294040ecb7d82587cc883fefeb808c55d13
Tupiet <Tupiet@users.noreply.github.com>
2020-05-13 19:14:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function canvi() {
-  document.body.style.backgroundColor = "#AA0000";
-  if (document.body.style.backgroundColor == "#AA0000") {
+  document.body.style.backgroundColor = red;
+  if (document.body.style.backgroundColor == red) {
     //alert("AA0000");
-    document.body.style.backgroundColor = "#00AA00";
+    document.body.style.backgroundColor = green;
     setTimeout(canvi(), 3000);
   } else {
     //alert("NO");
-    document.body.style.backgroundColor = "#AA0000";
+    document.body.style.backgroundColor = red;
     setTimeout(canvi(), 3000);
   }
 
