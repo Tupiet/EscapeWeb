a0428d1196d54cdb4bb59e90cc152ad1b2b9384f
Tupiet <Tupiet@users.noreply.github.com>
2020-05-13 17:18:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 function canvi() {
-  var id = document.getElementById("numberID");
-  if (id.text != 0) {
-    //alert("id != 0");
-    document.body.style.backgroundColor = "#AA0000";
-    id.text = 0;
-    setTimeout(canvi(), 1000);
-  }
-  if (id.text == 0) {
-    //alert("id = 0");
+
+  if (document.body.style.backgroundColor == "#AA0000") {
+    alert("AA0000");
     document.body.style.backgroundColor = "#00AA00";
-    id.text = 1;
+    setTimeout(canvi(), 1000);
+  } else {
+    alert("NO");
+    document.body.style.backgroundColor = "#AA0000";
     setTimeout(canvi(), 1000);
   }
 
