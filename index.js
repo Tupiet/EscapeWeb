9d104559e0e79c59b16a0cd4c558aed154085a37
Tupiet <Tupiet@users.noreply.github.com>
2020-05-13 16:46:17 +0200

    Updated

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 function canvi() {
   alert("Holaaa");
   var i = 1;
-  while (i == 1) {
+  if (i == 1) {
     document.body.style.backgroundColor = "#AA0000";
     i++;
-
+  } else {
+    document.body.style.backgroundColor = "#00AA00";
+    i = 1;
   }
+
+}
 
 }
