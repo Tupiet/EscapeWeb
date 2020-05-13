deaed7fa7d456ba0b8c1454f842e83d71ef73afe
Tupiet <Tupiet@users.noreply.github.com>
2020-05-13 19:19:49 +0200

    Updated

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 function canvi() {
-  document.body.style.backgroundColor = "#AA0000";
-  if (document.body.style.backgroundColor == "#AA0000") {
-    //alert("AA0000");
-    document.body.style.backgroundColor = "#00AA00";
-    setTimeout(canvi(), 3000);
-  } else {
-    //alert("NO");
-    document.body.style.backgroundColor = "#AA0000";
-    setTimeout(canvi(), 3000);
-  }
+  var backDiv = document.getElementById("backgroundDiv");
+  backgroundDiv.style.backgroundColor = "#AA00AA";
+
 
 }
