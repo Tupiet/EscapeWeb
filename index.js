8ad149528d50e79aed737e820f26092f103de0ac
Tupiet <Tupiet@users.noreply.github.com>
2020-05-13 17:12:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ function canvi() {
     alert("id != 0");
     document.body.style.backgroundColor = "#AA0000";
     id.text = 0;
+    canvi();
   }
   while (id.text == 0) {
     alert("id = 0");
     document.body.style.backgroundColor = "#00AA00";
     id.text = 1;
+    canvi();
   }
 
 }
