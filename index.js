a5522e019f3d45a961a1a262d9aa6f732943fbaa
Tupiet <Tupiet@users.noreply.github.com>
2020-05-13 19:56:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var i = 0;
 
 function canvi() {
   var doc = document.body;
-  var color = ["blue", "red"];
+  var color = ["#aa0000", "#000075"];
   doc.style.backgroundColor = color[i];
   i = (i + 1) % color.length;
 }
