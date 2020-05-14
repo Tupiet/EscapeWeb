d3e75c48dbefab035aec3127bb35d9fb430e25a0
Tupiet <Tupiet@users.noreply.github.com>
2020-05-14 15:55:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-var i = 0;
+var iBody = 0;
+var iDivVirus = 0;
 
 function canvi() {
   var bodyBack = document.body;
   var virusDiv = document.getElementById("virusAlert");
   var colorBody = ["#aa0000", "#000075"];
   var colorVirusDiv = ["#000075", "#aa0000"];
-  bodyBack.style.backgroundColor = colorBody[i];
-  i = (i + 1) % colorBody.length;
-  virusDiv.style.backgroundColor = colorVirusDiv[i];
-  i = (i + 1) % colorVirusDiv.length;
+  bodyBack.style.backgroundColor = colorBody[iBody];
+  iBody = (iBody + 1) % colorBody.length;
+  virusDiv.style.backgroundColor = colorVirusDiv[iDivVirus];
+  iDivVirus = (iDivVirus + 1) % colorVirusDiv.length;
 }
 
 setInterval(canvi, 1000);
