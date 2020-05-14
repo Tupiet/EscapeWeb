625a9ab8ab08b08fd95d900cf76ab66aa2b43d38
Tupiet <Tupiet@users.noreply.github.com>
2020-05-14 15:52:39 +0200

    Added change color to the VirusDiv

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ var i = 0;
 
 function canvi() {
   var bodyBack = document.body;
+  var virusDiv = document.getElementById("virusAlert");
   var colorBody = ["#aa0000", "#000075"];
+  var colorVirusDiv = ["#000075", "#aa0000"];
   bodyBack.style.backgroundColor = colorBody[i];
   i = (i + 1) % colorBody.length;
+  virusDiv.style.backgroundColor = colorVirusDiv[i];
+  i = (i + 1) % colorVirusDiv.length;
 }
 
 setInterval(canvi, 1000);
