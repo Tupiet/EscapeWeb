e8935b4b55553945b75ef7f779b7e1e2842f29a0
Tupiet <Tupiet@users.noreply.github.com>
2020-05-15 20:05:04 +0200

    Updated

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function displayStart() {
   var initDiv = document.getElementById("welcome");
   var startDiv = document.getElementById("start");
 
-  initDiv.style.display = none;
-  startDiv.style.display = inline;
+  initDiv.style.display = "none";
+  startDiv.style.display = "inline";
 }
