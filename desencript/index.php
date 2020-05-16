19fa8e644901fdf2672ba8bcc1f4d866973a954d
Tupiet <Tupiet@users.noreply.github.com>
2020-05-16 15:00:07 +0200

    Update index.php

--- desencript/index.php
+++ desencript/index.php
@@ -4,8 +4,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($u_name)) {
     PRINT <<<HERE
         <h1>:(</h1>
-        <p>Ups... ¡Parece que has olvidado poner tu <b>pingüino!</b></p>
-        <p>Por favor, regresa <b><a href="../">atrás</a></b> e introduce tu usuario.</p>
+        <p>Ups... ¡Parece que has olvidado poner tu <b>clave de desencriptar!</b></p>
+        <p>Por favor, regresa <b><a href="../">atrás</a></b> e introduce tu clave.</p>
 
         HERE;
         die();
