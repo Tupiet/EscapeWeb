39413f1869f9037fb25142ce64d7f2bdeb6cd527
Tupiet <Tupiet@users.noreply.github.com>
2020-05-16 14:57:57 +0200

    Update

--- desencript/index.php
+++ desencript/index.php
@@ -1,12 +1,15 @@
 <?php
-$u_name = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
-if (empty($u_name)) {
-  PRINT <<<HERE
-      <h1>:(</h1>
-      <p>Ups... ¡Parece que has olvidado poner tu <b>pingüino!</b></p>
-      <p>Por favor, regresa <b><a href="../">atrás</a></b> e introduce tu usuario.</p>
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $u_name = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
+  if (empty($u_name)) {
+    PRINT <<<HERE
+        <h1>:(</h1>
+        <p>Ups... ¡Parece que has olvidado poner tu <b>pingüino!</b></p>
+        <p>Por favor, regresa <b><a href="../">atrás</a></b> e introduce tu usuario.</p>
 
-      HERE;
-      die();
+        HERE;
+        die();
+  }
 }
+
  ?>
