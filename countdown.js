482accb1701d1648e36793dd102b04103afc5f12
Tupiet <Tupiet@users.noreply.github.com>
2020-06-03 12:48:51 +0200

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -21,9 +21,9 @@ function showRemaining() {
   var minutes = Math.floor((distance % _hour) / _minute);
   var seconds = Math.floor((distance % _minute) / _second);
 
-  document.getElementById('countdown').innerHTML = days + ' días,';
-  document.getElementById('countdown').innerHTML += hours + ' horas,';
-  document.getElementById('countdown').innerHTML += minutes + ' minutos,';
+  document.getElementById('countdown').innerHTML = days + ' días, ';
+  document.getElementById('countdown').innerHTML += hours + ' horas, ';
+  document.getElementById('countdown').innerHTML += minutes + ' minutos, ';
   document.getElementById('countdown').innerHTML += seconds + ' segundos';
 }
 
