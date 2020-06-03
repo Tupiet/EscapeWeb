7d496ae596d2added68f44fcfb11647e4a274db8
Tupiet <Tupiet@users.noreply.github.com>
2020-06-03 13:05:18 +0200

    Update

--- index.js
+++ index.js
@@ -18,3 +18,24 @@ function finalPassword() {
 
   }
 }
+
+var i = 0;
+
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 100);
+
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
